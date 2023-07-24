96c03b02008805147e8247c53ade0c605445bb5e
Your Name <you@example.com>
2023-07-24 15:56:28 +0700

    update v5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className='flex gap-4'>
-        <Button variant={'default'} className='rounded-full'>default</Button>
-        <Button variant={'destructive'}>destructive</Button>
-        <Button variant={'ghost'}>ghost</Button>
-        <Button variant={'link'}>link</Button>
-        <Button variant={'outline'}>outline</Button>
-        <Button variant={'secondary'} >secondary</Button>
-      </div>
+      
 
     </main>
   )
---
+++ src/pages/modelA/update/[id].tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps } from 'next'
+import React from 'react'
+
+type Props = {
+    id: number
+}
+
+export default function WorklogUpdate({ id }: Props) {
+    return (
+        <div>{id}</div>
+    )
+}
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    // const shouldRedirect = await checkPermission({ permissionCode: PERMISSION_CODES.ROLE_MANAGE, type: 'view' }, ctx.req.cookies[APP_SAVE_KEY.TOKEN_KEY])
+    return { props: { id: ctx.query.id } }
+
+}
+
