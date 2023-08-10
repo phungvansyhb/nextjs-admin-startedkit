da71ce88b1837d62a41083cd2cd70454835e0347
sybk99 <phungvansyhb@gmail.com>
2023-08-10 14:06:31 +0700

    add 404 , 500 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,22 +3,59 @@ import { Button } from '@/shared/components/common/ui/button'
 import useTrans from '@/shared/hooks/useTrans'
 import { BanIcon } from 'lucide-react'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
+
+const Img404 = "/404.svg";
 
 
 const Error404 = () => {
   const { trans } = useTrans()
 
   return (
-    <div className='flex flex-col w-full justify-center items-center'>
+    <>
       <Head>
         <title>{trans.page[404].pageTitle}</title>
       </Head>
-      <div className='font-mono text-[88px] font-bold'>404 </div>
-      <BanIcon size={42} />
-      <div className='font-mono text-base font-bold'>Trang không tồn tại </div>
-      <Button type="button" variant={'default'} className='mt-8'> <Link href={"/"}>{trans.common.gobackHome}</Link> </Button>
-    </div>
+      <section className="pt-45 lg:pt-50 xl:pt-55 pb-25 lg:pb-32.5 xl:pb-37.5 overflow-hidden flex justify-center w-full">
+        <div className="animate_top mx-auto max-w-[518px] text-center">
+          <Image
+            src={Img404}
+            alt="404"
+            className="mx-auto mb-7.5"
+            width={396}
+            height={156}
+          />
+
+          <h2 className="font-semibold text-2xl md:text-4xl text-black dark:text-white mb-5">
+            Trang không tồn tại
+          </h2>
+          <p className="mb-7.5">
+            Trang bạn đang tìm kiếm đã được di chuyển, xóa hoặc không tồn tại
+
+          </p>
+
+          <button className=" bg-black dark:bg-btndark hover:bg-blackho ease-in-out duration-300 font-medium text-white rounded-full px-6 py-3 mt-4">
+            <Link href={'/'} className="inline-flex gap-2.5 items-center">
+              Quay trở lại trang chủ
+              <svg
+                className="fill-white"
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.4767 6.16664L6.00668 1.69664L7.18501 0.518311L13.6667 6.99998L7.18501 13.4816L6.00668 12.3033L10.4767 7.83331H0.333344V6.16664H10.4767Z"
+                  fill=""
+                />
+              </svg>
+            </Link>
+          </button>
+        </div>
+      </section>
+    </>
 
   )
 }
--- src/shared/components/layouts/ErrorBoudary.tsx
+++ src/shared/components/layouts/ErrorBoudary.tsx
@@ -1,5 +1,7 @@
 import React, { Component, ErrorInfo, ReactNode } from "react";
 import { Button } from "../common/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
     children?: ReactNode;
@@ -8,7 +10,7 @@ interface Props {
 interface State {
     hasError: boolean;
 }
-
+const Img500 = "/500.svg";
 class ErrorBoundary extends Component<Props, State> {
     public state: State = {
         hasError: false
@@ -25,19 +27,46 @@ class ErrorBoundary extends Component<Props, State> {
 
     public render() {
         if (this.state.hasError) {
-            return (<div className="flex h-screen w-screen items-center justify-center">
+            return (
+                <section className="pt-45 lg:pt-50 xl:pt-55 pb-25 lg:pb-32.5 xl:pb-37.5 overflow-hidden">
+                    <div className="animate_top mx-auto max-w-[518px] text-center">
+                        <Image
+                            src={Img500}
+                            alt="404"
+                            className="mx-auto mb-7.5"
+                            width={396}
+                            height={156}
+                        />
 
-                <Button type="button" key="console" onClick={() => {
-                    window.history.back()
-                    this.setState({ hasError: false })
-                }
-                }>
-                    Go back
-                </Button>
+                        <h2 className="font-semibold text-2xl md:text-4xl text-black dark:text-white mb-5">
+                            Xảy ra lỗi
+                        </h2>
+                        <p className="mb-7.5">
+                            Máy chủ xảy ra lỗi, vui lòng thử lại hoặc liên hệ admin
 
+                        </p>
 
-
-            </div>)
+                        <button className=" bg-black dark:bg-btndark hover:bg-blackho ease-in-out duration-300 font-medium text-white rounded-full px-6 py-3 mt-4">
+                            <Link href={'/'} className="inline-flex gap-2.5 items-center">
+                                Quay trở lại trang chủ
+                                <svg
+                                    className="fill-white"
+                                    width="14"
+                                    height="14"
+                                    viewBox="0 0 14 14"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M10.4767 6.16664L6.00668 1.69664L7.18501 0.518311L13.6667 6.99998L7.18501 13.4816L6.00668 12.3033L10.4767 7.83331H0.333344V6.16664H10.4767Z"
+                                        fill=""
+                                    />
+                                </svg>
+                            </Link>
+                        </button>
+                    </div>
+                </section>
+            )
         }
 
         return this.props.children;
